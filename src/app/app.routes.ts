71613210bb2fe42f58fb7b8f476c49599497ca89
[samir danmari] <[samirdanmari9@gmail.com]>
2024-09-06 19:48:09 +0100

    Neccessary components added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
 import { FrontOfficeComponent } from './front-office/front-office.component';
 import { AccountComponent } from './account/account.component';
 import { HouseKeepingComponent } from './house-keeping/house-keeping.component';
 import { StoreComponent } from './store/store.component';
+import path from 'node:path';
 export const routes: Routes = [
+    {path: 'Admin', component: AdminComponent},
+    {path: 'FrontOffice', component: FrontOfficeComponent},
+    {path: 'Accounting', component: AccountComponent},
+    {path: 'HouseKeeping', component: HouseKeepingComponent},
+    {path: 'Store', component: StoreComponent},
  
 ];
